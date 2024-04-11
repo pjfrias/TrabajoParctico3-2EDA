1641e527659dd0bf0ddb606681e30cf02654072f
pjfrias <pjfrias@gmail.com>
2024-04-11 17:05:02 -0300

    Initial commit

---
+++ src/trabajoparctico3/pkg2eda/TrabajoParctico32EDA.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trabajoparctico3.pkg2eda;
+
+public class TrabajoParctico32EDA {
+
+    public static void main(String[] args) {
+     
+    }
+
+}
